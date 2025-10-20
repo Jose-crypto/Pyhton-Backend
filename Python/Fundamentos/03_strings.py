e786a0ef26c9493fe710fdac7dfd623fd458740b
JoseDev7 <josefr7h@gmail.com>
2025-10-20 16:43:42 -0300

    modified strings and create listas file

--- Python/Fundamentos/03_strings.py
+++ Python/Fundamentos/03_strings.py
@@ -9,3 +9,24 @@ print(my_string,my_other_string)
 #Salto de linea
 my_tab_strings="This is a string with \ncon salto de linea"
 print(my_tab_strings)
+
+#operators
+name , surname , age = "Jose", "Antonio",25
+print("My name is %s %s and my age is %d" %(name,surname,age))
+print("My name is {} {} and my age is {}" .format(name,surname,age))
+print(f"My name is {name} {surname} and my age is {age}") #best pratice
+
+
+#desempaquetado de caracteres
+language = 'Python'
+a,b,c,d,e,f = language
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+#division
+language_slice= language[1:4]
+print("Lenguaje cortado: ", language_slice)
---
+++ Python/Fundamentos/04_listas.py
@@ -0,0 +1,11 @@
+## lists ##
+
+#structured data 
+
+my_list= ['Apple','Banana','Kiwi']
+my_list.append('coco') # adding item to list
+my_list.reverse() # reversing list 
+print(my_list) # print te list modified
+
+my_second_list= list()
+print(my_second_list)
