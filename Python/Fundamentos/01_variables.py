4e27a507056ec69c33a882442d668ea08ef34f8e
JoseDev7 <josefr7h@gmail.com>
2025-10-20 13:19:19 -0300

    Add the new file about variables in python

---
+++ Python/Fundamentos/01_variables.py
@@ -0,0 +1,15 @@
+#variables
+
+first_name = "Jose"
+second_name= "Antonio"
+last_name="Espinoza"
+full_name="Jose Antonio Espinoza Franco"
+print(full_name)
+
+
+#int 
+variable_int= 5
+#boolean
+variable_boolean = True
+
+print(type(variable_boolean))
