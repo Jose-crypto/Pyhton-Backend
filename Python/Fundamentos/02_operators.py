c8829c7447aca1dcddb4572ff6b9120b0b5b4ea9
JoseDev7 <josefr7h@gmail.com>
2025-10-20 15:18:44 -0300

    amodified datos operators file

--- Python/Fundamentos/02_operators.py
+++ Python/Fundamentos/02_operators.py
@@ -7,3 +7,26 @@ x=5
 z=3
 z+=3
 print(z) 
+
+x-=4
+print(x)
+
+#arithmetics operators
+print(1+2)
+print(3-2)
+print(4*2)
+print(6/2)
+print(8%2)
+print(14//4)
+print(2**2)
+
+
+#logical operators
+
+print(3>7 and 2>7)
+print(32>7 or 4>4)
+
+#comparision operators
+print(3<8)
+print(20==4)
+print(1!=1)
