dfd7067d47c59be4fcba53f7c33c5f0493aa42d3
JoseDev7 <josefr7h@gmail.com>
2025-10-20 14:36:21 -0300

    Initial commit: Uploading project

--- Python/Fundamentos/01_variables.py
+++ Python/Fundamentos/01_variables.py
@@ -13,3 +13,30 @@ variable_int= 5
 variable_boolean = True
 
 print(type(variable_boolean))
+
+
+#concat the variables
+print("Nombre completo:", full_name)
+print("Nombre y apellido:",first_name, last_name,". Nombre Completo: ",full_name)
+
+#functions of the systems
+print(len(last_name))
+
+
+# inputs - entrada de datos
+age = input("How old are you: ")
+print("Your age is: ", age)
+
+
+##DICCIONARIO
+print('-----------------DICIONARIOS----------------')
+alumnos={
+    'Nombre': 'Jose Antonio',
+    'Apellido': 'Espinoza',
+    'age': 25
+}
+
+print(alumnos)
+#TUPLAS
+
+#SET
---
+++ Python/ejercicios/#2.py
@@ -0,0 +1,21 @@
+'''
+* Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+'''
+
+
+def is_anagram(first_word,second_word):
+    print('Primera palabra: ', first_word, ". Segunda palabra: ", second_word)
+
+
+
+
+
+
+first_word = input('Ingrese primera palabra: ')
+second_word = input('Ingrese segunda palabra: ')
+is_anagram( first_word,second_word)
